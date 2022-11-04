0103b09abd46ac78f9d97467b334eaf443fb80da
wahabu <ritscg2hibo@gmail.com>
2022-11-05 04:03:02 +0900

    Create folder about datatype in C++ to part_1 folder

---
+++ part_1/data_type.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // true, false
+    bool isOn = true;
+    
+    // only single char
+    char location = 'A';
+
+    // -32,768 ~ 32,767
+    short int counter;
+
+    // 0 ~ 65,535
+    unsigned short int counter2;
+
+    // -2,147,483,648 ~ 2,147,483,647
+    int counter3;
+
+    // 0 ~ 4,294,967,295
+    unsigned int counter4;
+
+    // -9223372036854775808 ~ 9223372036854775807
+    long int counter5;
+    
+    // 0 ~ 18446744073709551615
+    unsigned long int counter5;
+
+    // -(2^63) to (2^63)-1
+    long long int counter6;
+
+    // 0 to 18,446,744,073,709,551,615
+    unsigned long long int counter7;
+
+    //
+    signed char location2;
+
+    //
+    unsigned char location3;
+
+    //
+    float counter8;
+
+    //
+    double counter9;
+
+    //
+    long double counter10;
+
+    // 1 wide character
+    wchar_t location4;
+
+    return 0;
+}

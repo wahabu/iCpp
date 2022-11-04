a5dbb3dd18374873d2b9673ce4027057d8623476
wahabu <ritscg2hibo@gmail.com>
2022-11-04 10:58:57 +0900

    Create function file c++ code to part_1 folder

---
+++ part_1/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// declare a function
+void printMessageInConsole();
+
+int main() {
+    printMessageInConsole();
+    return 0;
+}
+
+void printMessageInConsole() {
+    cout << "Hello World!" << endl;
+}

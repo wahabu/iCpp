5a9f639168ef38d330281a113f51fae354ef577b
wahabu <ritscg2hibo@gmail.com>
2022-11-06 14:27:18 +0900

    Create file about variable to part_1 folder

---
+++ part_1/vars.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    float height = 177.7;
+    long mobile = 87654321; //phone number with DT of long not good idea
+    string country = "Saudi Arabia";
+}

6b4e488155dd6f1c5319ecb6cb83fc63da110095
wahabu <ritscg2hibo@gmail.com>
2022-11-06 23:52:02 +0900

    Create file about global scope and local scope to part_1 folder

---
+++ part_1/varScopes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// GLOBAL SCOPE
+int printAge();
+int age, mobile = 321, height = 177;
+
+int main() {
+    // LOCAL SCOPE
+    age = 12; // << global scope
+    cout << "global age: " << age << endl;
+    int age = 10; //  << local scope for main
+    cout << "main age: " << age << endl;
+    printAge();
+    cout << age << endl;
+    printAge();
+    cout << age << endl;
+    return 0;   
+}
+
+printAge() {
+    age = 11; // << global scope
+    cout << "global age: " << age << endl;
+    int age = 13; // << local scope for printAge
+    cout << "printAge age:" << age << endl;
+    return 0;
+}

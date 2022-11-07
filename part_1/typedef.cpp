c09cdac1bdb5752147fad65a9cb4ca1b5c97ad2e
wahabu <ritscg2hibo@gmail.com>
2022-11-08 02:46:15 +0900

    Creare file code how to rename a type of data

---
+++ part_1/typedef.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   // typedef, which you can use to give a type a new name
+   typedef unsigned long long ULL;
+   ULL idCard = 997755331122446688;
+   cout << sizeof(idCard) << endl;
+   return 0;
+}

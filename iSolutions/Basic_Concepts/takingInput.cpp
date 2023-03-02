419335f97518a4ce7f4d7bfb05e5bb56890a297f
wahabu <ritscg2hibo@gmail.com>
2023-03-02 16:18:26 +0900

    Solved takingInput challenge

---
+++ iSolutions/Basic_Concepts/takingInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  int b;
+  // your code goes here
+  int sum;
+
+  cin >> a >> b;
+  sum = a + b;
+  cout << sum << "\n";
+
+  return 0;
+}

5362e04948ffdcf47e40236d20b7a1b28deb2363
wahabu <ritscg2hibo@gmail.com>
2023-03-02 16:16:57 +0900

    Solved roomRenovation challenge

---
+++ iSolutions/Basic_Concepts/roomRenovation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // side of the room
+  double length;
+  double width;
+  cin >> length;
+  cin >> width;
+
+  // output the area
+  cout << length * width, '\n';
+
+  return 0;
+}

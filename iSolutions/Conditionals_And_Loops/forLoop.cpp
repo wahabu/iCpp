8a8d907e3de72985608da4fdd1613c1f7bb5a2e2
wahabu <ritscg2hibo@gmail.com>
2023-03-02 15:42:06 +0900

    Solved forLoop challenge

---
+++ iSolutions/Conditionals_And_Loops/forLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int distance = 0;
+  // your code goes here
+  for (int i = 1; i <= 5; i++)
+  {
+    cout << 40 * i << endl;
+  }
+
+  return 0;
+}

caaf1f1355acc1f8ef3fd9a086f08a9f99326fe4
wahabu <ritscg2hibo@gmail.com>
2023-03-02 16:16:25 +0900

    Solved robotCopyWriter challenge

---
+++ iSolutions/Basic_Concepts/robotCopyWriter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int count = 300; // letter in a minute
+  int time;
+  cin >> time;
+
+  // your code goes here
+  count *= time;
+  cout << count << endl;
+  return 0;
+}

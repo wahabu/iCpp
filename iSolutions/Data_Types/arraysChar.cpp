0cebc743b900bd7de325c37a528257bee80f4c11
wahabu <ritscg2hibo@gmail.com>
2023-03-11 23:54:17 +0900

    Create code about arrays char

---
+++ iSolutions/Data_Types/arraysChar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  char word[] = {'c', 'h', 'a', 'm', 'p', 'i', 'o', 'n', 's', 'h', 'i', 'p'};
+  // your code goes here
+  cout << word[0] << word[4] << word[11] << endl;
+
+  return 0;
+}

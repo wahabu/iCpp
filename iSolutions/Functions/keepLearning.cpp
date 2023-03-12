2cd88ad86c960018585c21a2ac63725c127d23d3
wahabu <ritscg2hibo@gmail.com>
2023-03-12 23:45:04 +0900

    Create the first Function code file

---
+++ iSolutions/Functions/keepLearning.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printSentence()
+{
+  // complete the function
+  cout << "Keep learning!" << endl;
+}
+
+int main()
+{
+  // function call
+  printSentence();
+  return 0;
+}

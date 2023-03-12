5028be98cd66b042ee59483806d241a260668cb9
wahabu <ritscg2hibo@gmail.com>
2023-03-12 23:51:23 +0900

    Create function converter H to min code file

---
+++ iSolutions/Functions/hoursToMinutes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void toMinutes(int hours)
+{
+  // complete the function
+  cout << hours * 60 << endl;
+}
+
+int main()
+{
+  // call the function
+  int H;
+  cin >> H;
+  toMinutes(H);
+
+  return 0;
+}

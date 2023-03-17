8bbf80082cc0ae33cb60b4ebc894c796d2c79a24
wahabu <ritscg2hibo@gmail.com>
2023-03-17 23:22:44 +0900

    Create new code file

---
+++ iSolutions/ClassesAndObjects/MuchVroom.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+// class definition
+class Car
+{
+
+  // private area
+private:
+  int horsepowers;
+
+  // public area
+public:
+  // complete the setter function
+  void setHorsepowers(int h)
+  {
+    if (h > 800)
+    {
+      cout << "Too much" << endl;
+      horsepowers = h;
+    }
+    else
+    {
+      horsepowers = h;
+    }
+  }
+
+  // complete the getter function
+  int getHorsepowers()
+  {
+    return horsepowers;
+  }
+};
+
+int main()
+{
+  // getting input
+  int horsepowers;
+  cin >> horsepowers;
+  // creating the object of class Car
+  Car car;
+  // setting the value for private member
+  car.setHorsepowers(horsepowers);
+  // printing the value of private member
+  cout << car.getHorsepowers();
+
+  return 0;
+}

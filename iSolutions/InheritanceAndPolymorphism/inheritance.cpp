f05c53fb8ae2de0503c2d966bd22ac58db01ddc6
wahabu <ritscg2hibo@gmail.com>
2023-04-02 23:49:53 +0900

    Create new code file

---
+++ iSolutions/InheritanceAndPolymorphism/inheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+  //  Shape () {};
+  void draw()
+  {
+    cout << "Drawing...";
+  }
+};
+// inherit from Shape
+class Rectangle : public Shape
+{
+private:
+  int width;
+  int height;
+
+public:
+  Rectangle(int w, int h) : width(w), height(h)
+  {
+    cout << w << "x" << h << endl;
+  };
+};
+
+int main()
+{
+  int x, y;
+  cin >> x >> y;
+  Rectangle d(x, y);
+  // call the draw() method
+  d.draw();
+}

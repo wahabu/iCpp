4ce9aaefe3c1168a17bb4eb048177d4ea09c921c
wahabu <ritscg2hibo@gmail.com>
2023-04-05 20:16:22 +0900

    Update classes.cpp file

--- OOP/classes.cpp
+++ OOP/classes.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 class Person
 {
+public:
   string name;
   string birthday;
   int age;
-  int returnAge()
+  void introduceSelf()
   {
+    cout << "Hello, my name is " << name << endl;
+    cout << "and I am " << age << " years old." << endl;
   }
 };
 
@@ -19,5 +22,17 @@ class Course
 };
 int main()
 {
+  Person student;
+  Person *teacher = new Person;
+
+  student.name = "Jone";
+  student.age = 21;
+
+  teacher->name = "Nakano";
+  teacher->age = 33;
+
+  student.introduceSelf();
+  teacher->introduceSelf();
+  delete teacher;
   return 0;
 }

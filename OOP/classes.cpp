1950c01af27bb37b7d2c50a833ff56efda6ab6fd
wahabu <ritscg2hibo@gmail.com>
2023-04-05 20:51:04 +0900

    Update classes.cpp file

--- OOP/classes.cpp
+++ OOP/classes.cpp
@@ -5,9 +5,12 @@ using namespace std;
 class Person
 {
 public:
+  // attributes
   string name;
   string birthday;
   int age;
+
+  // methods
   void introduceSelf()
   {
     cout << "Hello, my name is " << name << endl;
@@ -22,16 +25,26 @@ class Course
 };
 int main()
 {
+  // create class object
   Person student;
+
+  // another way to create class object
   Person *teacher = new Person;
 
+  // modify class object attributes
   student.name = "Jone";
   student.age = 21;
 
+  // another way of created class object should be like
+  // the folloing to modify class object attributes
   teacher->name = "Nakano";
   teacher->age = 33;
 
+  // call on class objects
   student.introduceSelf();
+
+  // another way of modified class object attrubutes should be like
+  // the folloing to call on class objects
   teacher->introduceSelf();
   delete teacher;
   return 0;

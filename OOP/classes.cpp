751cde82465b82b82283ba754b64420d02fb2a28
wahabu <ritscg2hibo@gmail.com>
2023-04-05 19:33:44 +0900

    Create new code file

---
+++ OOP/classes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// OOP -Object Oriented Programming
+
+class Person
+{
+  string name;
+  string birthday;
+  int age;
+  int returnAge()
+  {
+  }
+};
+
+class Course
+{
+  string title;
+  int level;
+};
+int main()
+{
+  return 0;
+}

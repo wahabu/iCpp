5cd81b9ebb91b1c25497e1904d6bc5679eb92369
wahabu <ritscg2hibo@gmail.com>
2023-04-06 20:08:18 +0900

    Create new code file

---
+++ iSolutions/Basic_Concepts/DrawingSnowflake.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string p1 = "  *  ";
+  string p2 = " *** ";
+  string p3 = "*****";
+
+  cout << p1 << endl;
+  cout << p2 << endl;
+  cout << p3 << endl;
+  cout << p2 << endl;
+  cout << p1 << endl;
+}

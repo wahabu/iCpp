5848bbe4321b663815ce875b33136667bad3c5cc
wahabu <ritscg2hibo@gmail.com>
2023-04-06 18:50:26 +0900

    Create new code file

---
+++ iSolutions/Basic_Concepts/SmileyFace.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string smiley = "      @@@@            @@@@\n     @@@@@@          @@@@@@\n     @@@@@@          @@@@@@\n      @@@@            @@@@\n\n\n\n@                             @\n @@                         @@\n   @@@                   @@@\n      @@@@@         @@@@@\n         @@@@@@@@@@@@@\n";
+  cout << smiley;
+}

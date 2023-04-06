87e2adf07b494455c3dd912df3019049747eec48
wahabu <ritscg2hibo@gmail.com>
2023-04-06 19:53:52 +0900

    Create new code file

---
+++ iSolutions/Basic_Concepts/ContactInfo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string name = "James";
+  int age = 42;
+  float height = 185.7f;
+
+  cout << "Name: " << name << endl;
+  cout << "Age: " << age;
+  cout << " | Height: " << height;
+
+  return 0;
+}

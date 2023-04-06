352f9705c1c4c9b1271d0c2b3107d2f9db27983b
wahabu <ritscg2hibo@gmail.com>
2023-04-06 20:11:05 +0900

    Create new code file

---
+++ iSolutions/Basic_Concepts/LessonCompleted.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  string lesson = "Data Types";
+  bool isCompleted = true;
+
+  cout << "Lesson: " << lesson << endl;
+  cout << "Completed: " << isCompleted << endl;
+}

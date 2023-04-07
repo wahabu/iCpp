90f173f8c604de9e8d70049db743a643e3e1531b
wahabu <ritscg2hibo@gmail.com>
2023-04-07 21:03:36 +0900

    Country Cards code

---
+++ iSolutions/Conditionals_And_Loops/CountryCards.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string country, capital;
+
+  // your code goes here
+  cin >> country >> capital;
+
+  cout << "Country: " << country << endl;
+  cout << "Capital: " << capital << endl;
+
+  return 0;
+}

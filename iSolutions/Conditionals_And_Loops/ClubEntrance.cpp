078316ca2f53244829e464f64db534231636f1fc
wahabu <ritscg2hibo@gmail.com>
2023-04-08 22:36:17 +0900

    Club Entrance code

---
+++ iSolutions/Conditionals_And_Loops/ClubEntrance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // your code goes here
+  int age;
+  cin >> age;
+  if (age >= 16)
+  {
+    cout << "Welcome" << endl;
+  }
+  else
+  {
+    cout << "Not allowed" << endl;
+  }
+
+  return 0;
+}

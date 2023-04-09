e5a2e510e61b4d841512f5d77cd81aeb5395d110
wahabu <ritscg2hibo@gmail.com>
2023-04-09 21:36:46 +0900

    Coffee Machine

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    cin >> choice;
+
+    /* Coffee Types:
+    1. Espresso
+    2. Americano
+    3. Cappuccino
+    4. Latte
+    */
+    switch(choice){
+        case 1:
+            cout << "Espresso";
+            break;
+        case 2:
+            cout << "Americano";
+            break;
+        case 3:
+            cout << "Cappuccino";
+            break;
+        case 4:
+            cout << "Latte";
+            break;
+    }
+    return 0;
+}

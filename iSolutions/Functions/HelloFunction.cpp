1f8bc7ea62ad2d33ce023bac7292073e48ac5f22
wahabu <ritscg2hibo@gmail.com>
2023-04-20 22:33:33 +0900

    Sample function code

---
+++ iSolutions/Functions/HelloFunction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void hello()
+{
+  cout << "Hello!" << endl;
+  cout << "I am a sample function" << endl;
+}
+
+int main()
+{
+  hello();
+
+  return 0;
+}

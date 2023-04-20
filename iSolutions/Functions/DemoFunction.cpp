8d4a45fe5790ffadab4dd37597a219054626d7cb
wahabu <ritscg2hibo@gmail.com>
2023-04-20 22:44:05 +0900

    Calling a function code

---
+++ iSolutions/Functions/DemoFunction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Put the declaration of function before main
+void demo()
+{
+  cout << "This is a demo";
+}
+
+int main()
+{
+  // Call a funtion as many times as necessary
+  demo();
+  demo();
+  demo();
+
+  return 0;
+}

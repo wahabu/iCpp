dab99c2d3199af9deff57d7fe06f8f013f998dad
wahabu <ritscg2hibo@gmail.com>
2023-04-20 22:50:35 +0900

    Loading... function code

---
+++ iSolutions/Functions/LoadingFunction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Define the loading() function
+void loading()
+{
+  cout << "Loading..." << endl;
+}
+
+int main()
+{
+  // call the function
+  loading();
+
+  return 0;
+}

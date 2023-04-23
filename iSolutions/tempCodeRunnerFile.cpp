55070ba517735075dd4bbea565d9480a411b7f99
wahabu <ritscg2hibo@gmail.com>
2023-04-23 23:48:30 +0900

    Days To Seconds app calculate

---
+++ iSolutions/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Define toSeconds() function here
+int toSeconds(int days) {
+  return days * 24 * 60 * 60;
+}
+
+int main() {
+  int days;
+  cin >> days;
+
+  int seconds = toSeconds(days);
+  cout << seconds;
+}
